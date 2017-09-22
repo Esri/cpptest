8ac8e5b2e2ab3cb6eeec6056833a7640204046ac
Annette Locke <alocke@esri.com>
2017-09-22 11:49:50 -0700

    Revert "Sergey/xmloutput"

--- cpptest/src/cpptest.h
+++ cpptest/src/cpptest.h
@@ -37,8 +37,6 @@
 #include "cpptest-compileroutput.h"
 #include "cpptest-htmloutput.h"
 #include "cpptest-textoutput.h"
-#include "cpptest-simplecollector.h"
-#include "cpptest-xmloutput.h"
 
 #endif // #ifndef CPPTEST_H
 
